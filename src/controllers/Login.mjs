c3937d2d108d31bda24401009febc224b7efc4f4
VirajChoramale <99421517+VirajChoramale@users.noreply.github.com>
2024-07-08 13:01:27 +0530

    7/8

--- src/controllers/Login.mjs
+++ src/controllers/Login.mjs
@@ -1,103 +1,111 @@
-import { bcrypt_text,compare_bcrypt} from "../utility/bcrypt_js.mjs";
-import jsonwebtoken from 'jsonwebtoken';
+import { bcrypt_text, compare_bcrypt } from "../utility/bcrypt_js.mjs";
+import jsonwebtoken from "jsonwebtoken";
 import { sendOtpSMS } from "../utility/otp.mjs";
 import { select_from_table } from "../utility/Sql_Querries.mjs";
-const HmacKey=process.env.HMAC;
-import { executeReadQuery,executeWriteQuery} from "../db/db_operation.mjs";
+const HmacKey = process.env.HMAC;
+import { executeReadQuery, executeWriteQuery } from "../db/db_operation.mjs";
 import { update_table } from "../utility/Sql_Querries.mjs";
-const verify_user= async(username,password)=>{
-    const user=`select username,password,email,mobile from users_new where username='${username}'`; 
-    try {
-        const result=await executeReadQuery(user); 
-      
-        if (result.length>0){
-            
-                const authinticate=compare_bcrypt(password,result[0]['password']);
-             
-                if(authinticate){
-                
-                    return {
-                        "res":true,
-                        "data":result,
-                        "msg":"Authentication Successfull",
-                        "code":200
+const verify_user = async (username, password) => {
+  const user = `select username,password,email,mobile from users_new where username='${username}'`;
+  try {
+    const result = await executeReadQuery(user);
 
+    if (result.length > 0) {
+      const authinticate = compare_bcrypt(password, result[0]["password"]);
 
-                    }
-                }
-                else{
-                    return {
-                        "res":false,
-                        "data":null,
-                        "msg":"Invalid Credentials!!",
-                        "code":501
-                    };
-                }
-        }else{
-              return {
-                "res":false,
-                "data":null,
-                "msg":"Invalid Credentials!!",
-                "code":502
-
-            };
-        }
-            
-    } catch (error) {
-        console.log(error)
+      if (authinticate) {
+        return {
+          res: true,
+          data: result,
+          msg: "Authentication Successfull",
+          code: 200,
+        };
+      } else {
+        return {
+          res: false,
+          data: null,
+          msg: "Invalid Credentials!!",
+          code: 501,
+        };
+      }
+    } else {
+      return {
+        res: false,
+        data: null,
+        msg: "Invalid Credentials!!",
+        code: 502,
+      };
     }
-  
-   
-
-}
-const login=async(req,res,next)=>{
-   const resp=await verify_user(req.body.username,req.body.password);
-   //console.log(req.body.username,req.body.password);
-  if(resp.code==200){
+  } catch (error) {
+    console.log(error);
+  }
+};
+const login = async (req, res, next) => {
+  const resp = await verify_user(req.body.username, req.body.password);
+  //console.log(req.body.username,req.body.password);
+  if (resp.code == 200) {
     const uname = resp.data[0].username;
-   const token=jsonwebtoken.sign({uname},HmacKey,{expiresIn:"120m",algorithm:"HS256"});
-   await sendOtpSMS(resp.data[0].mobile,resp.data[0].username).then((res)=>{}).catch((e)=>console.log("error in sms api"+e));
-   
-  //console.log(resp.data[0].mobile)
-   res.cookie("tid",token,{expire: 15000 + Date.now(),httpOnly: true, 
-    secure: true, 
-    sameSite: 'None' })
-   res.status(200).send({"msg":resp.msg,"res":resp.res,"lid":token})
-   next();
+    const token = jsonwebtoken.sign({ uname }, HmacKey, {
+      expiresIn: "120m",
+      algorithm: "HS256",
+    });
+    let sms_resp = null;
+    const resp_arr = {};
+    await sendOtpSMS(resp.data[0].mobile, resp.data[0].username)
+      .then((res) => {
+        const resp_arr = res.split(",");
+        sms_resp = resp_arr[0];
+      })
+      .catch((e) => console.log("error in sms api" + e));
 
-    
-   }
-   else{
-    res.json(resp)
-   }
- 
-}
+    //console.log(resp.data[0].mobile)
+    res.cookie("tid", token, {
+      expire: 15000 + Date.now(),
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
 
-const verify_otp=async(req,res)=>{
-    const tes=await executeReadQuery(`select latest_otp,password,role from users_new where username='${req.user.uname}'`)
-    if(tes[0].latest_otp==req.body.otp){
-        const token =jsonwebtoken.sign({"uname":req.user.uname,"role":tes[0].role,"cred":tes[0].password},process.env.HMAC);
-        const sql=await update_table("users_new","username",req.user.uname,['latest_otp'],[""])
-
-        
-        res.cookie("eid",token);
-        res.status(200).json({
-            "msg":"Validated",
-             "role":tes[0].role,
-             'lid':token
-
-            
-        });
-    
+    if (sms_resp === "402") {
+      resp_arr.msg = `OTP has been successfully sent on ${resp.data[0].mobile} and ${resp.data[0].email}`;
+    } else {
+      resp_arr.msg = `ERROR!! while sending the OTP, Kindly Contact DTE-IT Cell`;
     }
-    else{
-        res.status(200).json({
-            "msg":"Invalid_Otp",
-             "code":"401"
+    res.status(200).send({ msg: resp_arr.msg, res: resp.res, lid: token });
+    next();
+  } else {
+    res.json(resp);
+  }
+};
 
-            
-        })
-    }
-    
-}
-export {login,verify_otp,verify_user};
+const verify_otp = async (req, res) => {
+  const tes = await executeReadQuery(
+    `select latest_otp,password,role from users_new where username='${req.user.uname}'`
+  );
+  if (tes[0].latest_otp == req.body.otp) {
+    const token = jsonwebtoken.sign(
+      { uname: req.user.uname, role: tes[0].role, cred: tes[0].password },
+      process.env.HMAC
+    );
+    const sql = await update_table(
+      "users_new",
+      "username",
+      req.user.uname,
+      ["latest_otp"],
+      [""]
+    );
+
+    res.cookie("eid", token);
+    res.status(200).json({
+      msg: "Validated",
+      role: tes[0].role,
+      lid: token,
+    });
+  } else {
+    res.status(200).json({
+      msg: "Invalid_Otp",
+      code: "401",
+    });
+  }
+};
+export { login, verify_otp, verify_user };
--- src/utility/otp.mjs
+++ src/utility/otp.mjs
@@ -1,79 +1,85 @@
-
-import crypto from "crypto"
+import crypto from "crypto";
 import https from "https";
-import  qs  from "qs";
+import qs from "qs";
 import { update_table } from "./Sql_Querries.mjs";
 
-
-function generateOtp(){
-    const length=Math.floor(Math.random() * 3) + 2;
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
+function generateOtp() {
+  const length = Math.floor(Math.random() * 3) + 2;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
   const charactersLength = characters.length;
-  for (let i = 0; i <5; i++) {
+  for (let i = 0; i < 5; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
-const username = 'dtemumbai';
-const password = 'Dtems#456@123';
-const deptSecureKey = 'b939a112-d103-4469-b22b-3c80b65328e5';
-const senderid = 'DTEMUM';
-const templateid='1307170668791675721'
+const username = "dtemumbai";
+const password = "Dtems#456@123";
+const deptSecureKey = "b939a112-d103-4469-b22b-3c80b65328e5";
+const senderid = "DTEMUM";
+const templateid = "1307170668791675721";
 function generateKey(message) {
   return crypto
-    .createHash('sha512')
+    .createHash("sha512")
     .update(`${username}${senderid}${message}${deptSecureKey}`)
-    .digest('hex');
+    .digest("hex");
 }
 
+async function sendOtpSMS(mobileno, uname) {
+  const OTP = generateOtp();
+  const message = `Dear S/M, OTP for EMIS is :  ${OTP} Thank you , DTE, Mumbai`;
 
-async function sendOtpSMS( mobileno,uname) {
-   
-    const OTP=generateOtp();
-    const message=`Dear S/M, OTP for EMIS is :  ${OTP} Thank you , DTE, Mumbai`
-
-  const encryp_password = crypto.createHash('sha1').update(password).digest('hex');
+  const encryp_password = crypto
+    .createHash("sha1")
+    .update(password)
+    .digest("hex");
   const key = generateKey(message);
-  const sql=await update_table("users_new","username",uname,['latest_otp'],[`${OTP}`])
+  const sql = await update_table(
+    "users_new",
+    "username",
+    uname,
+    ["latest_otp"],
+    [`${OTP}`]
+  );
   const data = {
     username,
     password: encryp_password,
     senderid,
     content: message,
-    smsservicetype: 'otpmsg',
+    smsservicetype: "otpmsg",
     mobileno,
     key,
     templateid,
   };
 
-  const postData = qs.stringify(data); 
+  const postData = qs.stringify(data);
 
   const options = {
-    hostname: 'msdgweb.mgov.gov.in',
-    path: '/esms/sendsmsrequestDLT',
-    method: 'POST',
+    hostname: "msdgweb.mgov.gov.in",
+    path: "/esms/sendsmsrequestDLT",
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': postData.length,
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Content-Length": postData.length,
     },
     // Enable SSL verification for production (remove rejectUnauthorized if using a trusted certificate)
     rejectUnauthorized: false,
   };
-  
+
   return new Promise((resolve, reject) => {
     const req = https.request(options, (res) => {
-      let responseBody = '';
-      res.on('data', (chunk) => {
+      let responseBody = "";
+      res.on("data", (chunk) => {
         responseBody += chunk;
       });
 
-      res.on('end', () => {
+      res.on("end", () => {
         resolve(responseBody);
       });
     });
 
-    req.on('error', (error) => {
+    req.on("error", (error) => {
       reject(error);
     });
 
@@ -82,4 +88,4 @@ async function sendOtpSMS( mobileno,uname) {
   });
 }
 
-export  { sendOtpSMS }; // Export the function as a module
+export { sendOtpSMS }; // Export the function as a module
