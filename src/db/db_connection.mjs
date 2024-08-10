0f0c6500c89a26adf40a3d0d5fa5ab6c6531cdf2
VirajChoramale <99421517+VirajChoramale@users.noreply.github.com>
2024-08-10 14:17:18 +0530

    env update db

--- src/db/db_connection.mjs
+++ src/db/db_connection.mjs
@@ -10,7 +10,7 @@ const primary_connection = createPool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      database: process.env.MODE=="PROD"?process.env.DB_PROD:process.env.DB_DEV,
       dateStrings: true,
     }),
   destroy: (conn) => conn.end(),
